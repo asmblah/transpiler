354efa918a6f5ea685b01fc1a96b4e8d6949ff05
Dan Phillimore <dan@ovms.co>
2026-07-10 17:35:26 +0100

    Optimise transpilation

--- src/Transpiler.js
+++ src/Transpiler.js
@@ -10,7 +10,70 @@
 'use strict';
 
 var _ = require('microdash'),
-    hasOwn = {}.hasOwnProperty;
+    hasOwn = {}.hasOwnProperty,
+    /**
+     * Internal recursive transpile, called with already-normalised options.
+     * Skips the options setup that the public transpile() does, so recursive
+     * calls (via the interpret() closure) don't pay that cost on every node.
+     */
+    transpileNode = function (transpiler, node, data, options) {
+        var nodeName,
+            spec = transpiler.spec,
+            transpileFn,
+            interpret;
+
+        if (!hasOwn.call(node, 'name')) {
+            throw new Error('Transpiler.transpile() :: Invalid AST node provided');
+        }
+
+        nodeName = node.name;
+
+        if (!hasOwn.call(spec.nodes, nodeName) && (!options || !hasOwn.call(options.nodes, nodeName))) {
+            throw new Error(
+                'Transpiler.transpile() :: Spec does not define how to handle node "' + nodeName + '"'
+            );
+        }
+
+        transpileFn = (options && options.nodes[nodeName]) || spec.nodes[nodeName];
+
+        // interpret() is the primary closure passed as the 2nd argument to every handler.
+        // Using `=== undefined` instead of `arguments.length === 1` avoids materialising
+        // the arguments object, allowing V8 to optimise the closure more aggressively.
+        interpret = function (node, newData) {
+            if (newData === undefined) {
+                newData = data;
+            } else if (newData && (typeof newData === 'object')) {
+                newData = _.extend({}, data, newData);
+            }
+
+            return typeof node === 'string' ? node : transpileNode(transpiler, node, newData, options);
+        };
+
+        // The 4th argument (original) is only created when the handler declares it, checked via
+        // Function.length. This avoids allocating a second closure on every call for the common
+        // case where handlers only use (node, interpret, context).
+        if (transpileFn.length >= 4) {
+            return transpileFn.call(
+                transpiler,
+                node,
+                interpret,
+                data,
+                function (node, newData) {
+                    if (newData === undefined) {
+                        newData = data;
+                    } else if (newData && (typeof newData === 'object')) {
+                        newData = _.extend({}, data, newData);
+                    }
+
+                    // Pass null options so that node overrides are not applied -
+                    // the handler gets the original spec behaviour.
+                    return typeof node === 'string' ? node : transpileNode(transpiler, node, newData, null);
+                }
+            );
+        }
+
+        return transpileFn.call(transpiler, node, interpret, data);
+    };
 
 function Transpiler(spec) {
     this.spec = spec;
@@ -18,11 +81,6 @@ function Transpiler(spec) {
 
 _.extend(Transpiler.prototype, {
     transpile: function (node, data, options) {
-        var transpiler = this,
-            transpileFn,
-            nodeName,
-            spec = transpiler.spec;
-
         if (!options) {
             options = {};
         }
@@ -31,47 +89,11 @@ _.extend(Transpiler.prototype, {
             options.nodes = {};
         }
 
-        if (!hasOwn.call(node, 'name')) {
-            throw new Error('Transpiler.transpile() :: Invalid AST node provided');
-        }
-
         if (arguments.length === 1) {
             data = null;
         }
 
-        nodeName = node.name;
-
-        if (!hasOwn.call(spec.nodes, nodeName) && !hasOwn.call(options.nodes, nodeName)) {
-            throw new Error(
-                'Transpiler.transpile() :: Spec does not define how to handle node "' + nodeName + '"'
-            );
-        }
-
-        transpileFn = options.nodes[nodeName] || spec.nodes[nodeName];
-
-        function createSubTranspile(useOriginal) {
-            return function (node, newData) {
-                if (arguments.length === 1) {
-                    newData = data;
-                } else if (newData && (typeof newData === 'object')) {
-                    newData = _.extend({}, data, newData);
-                }
-
-                if (_.isString(node)) {
-                    return node;
-                } else {
-                    return transpiler.transpile(node, newData, useOriginal ? options : null);
-                }
-            };
-        }
-
-        return transpileFn.call(
-            transpiler,
-            node,
-            createSubTranspile(true),
-            data,
-            createSubTranspile(false)
-        );
+        return transpileNode(this, node, data, options);
     }
 });
 
